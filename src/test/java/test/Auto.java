43c95c50275e62714287a74a236f9a8c7368bc49
Andrea-Merino-Mesa <amerino@unal.edu.co>
2024-11-08 19:49:41 -0500

    prueba1

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,25 @@
+package test;
+
+public class Auto {
+    String modelo;
+    int precio;
+    Asiento [] asientos;
+    String marca;
+    Motor motor;
+    int registro;
+    static String cantidadCreados;
+    int cantidadAsientos(){
+    int casientos=0;
+       for(int i=0;i<asientos.length;i++) {
+    	   if(asientos[i]!=null) {
+    		   casientos++;
+    	   }
+       }
+       return casientos;
+    }
+    String verificarIntegridad(){
+    	if(registro!=motor.registro) {
+        return "Las piezas no son originales";}
+        return "Auto original";
+     }
+}
---
+++ src/test/java/test/Motor.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Motor {
+    int numeroCilindros;
+    String tipo;
+    int registro;
+    void cambiarRegistro(int nuevoregistro){
+        registro=nuevoregistro;
+     }
+     void asignarTipo(String nuevotipo){
+        if(nuevotipo=="electrico" || nuevotipo=="gasolina"){
+            tipo=nuevotipo;
+        }
+      }
+}
